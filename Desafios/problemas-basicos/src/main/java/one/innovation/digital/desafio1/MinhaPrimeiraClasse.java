1c4c8ebc57403c4699b46e57f496d06c14a83c50
OtavioKoike <otaviokoike07@gmail.com>
2021-04-22 10:09:38 -0300

    Finalizado Desafio Problemas Basicos

--- Desafios/problemas-basicos/src/main/java/one/innovation/digital/desafio1/MinhaPrimeiraClasse.java
+++ Desafios/problemas-basicos/src/main/java/one/innovation/digital/desafio1/MinhaPrimeiraClasse.java
@@ -1,4 +1,29 @@
 package one.innovation.digital.desafio1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class MinhaPrimeiraClasse {
+    public static void main(String[] args) throws IOException {
+//        Para Plataforma (Descomentar)
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int a = Integer.parseInt(st.nextToken());
+
+//        st = new StringTokenizer(br.readLine());
+//        int b = Integer.parseInt(st.nextToken());
+
+//      Para Teste Local (Comentar)
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite dois numeros:");
+        int a = ler.nextInt();
+        int b = ler.nextInt();
+
+//      Ambos (Deixar)
+        int total = a * b; // Altere o valor da variável com o cálculo esperado
+        System.out.println("PROD = " + total);
+    }
 }
--- Desafios/problemas-basicos/src/main/java/one/innovation/digital/desafio3/MinhaTerceiraClasse.java
+++ Desafios/problemas-basicos/src/main/java/one/innovation/digital/desafio3/MinhaTerceiraClasse.java
@@ -1,2 +1,14 @@
-package one.innovation.digital.desafio3;public class MinhaTerceiraClasse {
+package one.innovation.digital.desafio3;
+
+import java.util.Scanner;
+
+public class MinhaTerceiraClasse {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num = entrada.nextInt();
+        for (int i = 0; i <= num; i++) {
+            if(i % 2 != 0)
+                System.out.println(i);
+        }
+    }
 }
